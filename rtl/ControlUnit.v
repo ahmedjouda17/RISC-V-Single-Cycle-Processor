78ccd02ea9bf5e176f13e8a3fe83cb031ab25591
ahmedjouda17 <ahmedjouda171@gmail.com>
2026-07-25 23:11:11 +0300

    ControlUnit module for instruction decoding

--- rtl/ControlUnit.v
+++ rtl/ControlUnit.v
@@ -1,62 +1,86 @@
 module ControlUnit (
-    input  [6:0] opcode,
+    input  [6:0]     opcode,
     output reg       MemRead,
     output reg       MemWrite,
-    output reg       MemToReg,
+    output reg       ResultSrc,
     output reg       Branch,
     output reg       ALUSrc,
     output reg       RegWrite,
-    output reg [1:0] ALUOp
+    output reg [1:0] ALUOp,
+    output reg       load,
+    output reg [1:0] ImmSrc
 );
  
     always @(*) begin
         // Default values
-        MemRead  = 0;
+        Branch =0;
         MemWrite = 0;
-        MemToReg = 0;
+        ResultSrc =0;
         Branch   = 0;
         ALUSrc   = 0;
         RegWrite = 0;
         ALUOp    = 2'b00;
- 
+        ImmSrc=2'b00;
+        load =1;
+ if (|opcode)begin 
+ load=1;
         case (opcode)
-            // R-TYPE (0110011)
-            7'b0110011: begin
+                    // load (000_0011)
+            7'b000_0011:begin
+                   Branch =0;
+                   MemWrite = 0;
+                   ResultSrc =1;
+                   Branch   = 0;
+                   ALUSrc   = 1;
+                   RegWrite = 1;
+                   ALUOp    = 2'b00;
+                   ImmSrc = 2'b00;
+                   end 
+            // R-TYPE (011_0011)
+            7'b011_0011: begin
                 RegWrite = 1;
                 ALUSrc   = 0;
                 ALUOp    = 2'b10;
+                 Branch =0;
+                 ResultSrc =0;
+                 MemWrite = 0;
+                 
             end
  
-            // I-TYPE (ADDI = 0010011)
-            7'b0010011: begin
+            // I-TYPE (ADDI = 001_0011)
+            7'b001_0011: begin
                 RegWrite = 1;
                 ALUSrc   = 1;
                 ALUOp    = 2'b10;
+                ImmSrc = 2'b00;      
+                Branch =0;
+                ResultSrc =0;
+                MemWrite = 0;
             end
  
-            // LOAD (LW = 0000011)
-            7'b0000011: begin
-                MemRead  = 1;
-                RegWrite = 1;
-                ALUSrc   = 1;
-                MemToReg = 1;
+            // STORE (SW = 0100_011)
+            7'b010_0011: begin
+               RegWrite = 0;
+               ALUSrc   = 1;
                 ALUOp    = 2'b00;
+                ImmSrc = 2'b01;      
+                 Branch =0;
+                  MemWrite = 1;
             end
  
-            // STORE (SW = 0100011)
-            7'b0100011: begin
-                MemWrite = 1;
-                ALUSrc   = 1;
-                ALUOp    = 2'b00;
-            end
- 
-            // BRANCH (BEQ = 1100011)
-            7'b1100011: begin
-                Branch   = 1;
-                ALUSrc   = 0;
-                ALUOp    = 2'b01;
+            // BRANCH (BEQ = 110_0011)
+            7'b110_0011: begin
+                 RegWrite = 0;
+                 ALUSrc   = 0;
+                 ALUOp    = 2'b01;
+                 ImmSrc = 2'b10;      
+                 Branch =1;
+                 MemWrite = 0;
             end
         endcase
+        end else begin 
+        load =0;
     end
+  end
  
 endmodule
