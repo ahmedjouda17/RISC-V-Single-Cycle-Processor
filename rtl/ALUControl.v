37e515dd5454c836fe2b28345f5031e74611cb16
ahmedjouda17 <ahmedjouda171@gmail.com>
2026-07-25 23:05:03 +0300

    ALUControl
    Refactored ALUControl logic to utilize casex for better handling of ALU operations based on ALUOp, funct3, OP5, and funct7 inputs.

--- rtl/ALUControl.v
+++ rtl/ALUControl.v
@@ -1,39 +1,37 @@
 module ALUControl (
     input      [1:0] ALUOp,
     input      [2:0] funct3,
-    input      [6:0] funct7,
+    input      funct7,
+    input      OP5,
     output reg [2:0] ALUControl
 );
  
-    always @(*) begin
-        case (ALUOp)
-            2'b00: ALUControl = 3'b000;   // ADD for LW/SW
-            2'b01: ALUControl = 3'b010;   // SUB for BEQ
- 
-            2'b10: begin
-                case (funct3)
-                    3'b000: begin
-                        if (funct7[5] == 1'b1)
-                            ALUControl = 3'b010;   // SUB
-                        else
-                            ALUControl = 3'b000;   // ADD
-                    end
-                    3'b111: ALUControl = 3'b111;   // AND
-                    3'b110: ALUControl = 3'b110;   // OR
-                    3'b100: ALUControl = 3'b100;   // XOR
-                    3'b001: ALUControl = 3'b001;   // SLL
-                    3'b101: begin
-                        if (funct7[5] == 1'b1)
-                            ALUControl = 3'b011;   // SRA
-                        else
-                            ALUControl = 3'b101;   // SRL
-                    end
-                    default: ALUControl = 3'b000;
-                endcase
-            end
- 
-            default: ALUControl = 3'b000;
-        endcase
-    end
+   always @(*) begin
+    ALUControl = 3'b000; // Default value
+
+    casex({ALUOp, funct3, OP5, funct7})
+
+        7'b00_xxx_xx: ALUControl = 3'b000; // ADD
+
+        7'b01_000_xx: ALUControl = 3'b010; // SUB
+        7'b01_001_xx: ALUControl = 3'b010; // SUB
+        7'b01_100_xx: ALUControl = 3'b010; // SUB
+
+        7'b10_000_00: ALUControl = 3'b000; // ADD
+        7'b10_000_10: ALUControl = 3'b000; // ADD
+        7'b10_000_01: ALUControl = 3'b000; // ADD
+
+        7'b10_000_11: ALUControl = 3'b010; // SUB
+
+        7'b10_001_xx: ALUControl = 3'b001; // SHL
+        7'b10_100_xx: ALUControl = 3'b100; // XOR
+        7'b10_101_xx: ALUControl = 3'b101; // SHR
+        7'b10_110_xx: ALUControl = 3'b110; // OR
+        7'b10_111_xx: ALUControl = 3'b111; // AND
+
+        default: ALUControl = 3'b000; // Default case
+
+    endcase
+end
  
 endmodule
